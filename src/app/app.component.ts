6ef253c49d2b3eb99cfee786c34c2df6335a22c5
Christian-Mirt <cmirt001@odu.edu>
2025-01-25 23:00:01 -0500

    Basic authentication and profile fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyAuthService } from './services/spotify-auth.service';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,47 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Spotify-Helper';
+
+export class AppComponent implements OnInit{
+  clientId: string = ''; // Add your client ID
+  accessToken: string | null = null;
+
+  constructor(private spotifyAuth: SpotifyAuthService) {}
+
+  async ngOnInit(): Promise<void> {
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get("code");
+    
+    if (!code) {
+        this.spotifyAuth.redirectToAuthCodeFlow(this.clientId);
+    } else {
+        const accessToken = await this.spotifyAuth.getAccessToken(this.clientId, code);
+        const profile = await this.fetchProfile(accessToken);
+        this.populateUI(profile);
+    }
+  }
+
+  async fetchProfile(token: string): Promise<any> {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    return await result.json();
+  }
+
+  populateUI(profile: any) {
+    document.getElementById("displayName")!.innerText = profile.display_name;
+    if (profile.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = profile.images[0].url;
+        document.getElementById("avatar")!.appendChild(profileImage);
+    }
+    document.getElementById("id")!.innerText = profile.id;
+    document.getElementById("email")!.innerText = profile.email;
+    document.getElementById("uri")!.innerText = profile.uri;
+    document.getElementById("uri")!.setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url")!.innerText = profile.href;
+    document.getElementById("url")!.setAttribute("href", profile.href);
+    document.getElementById("imgUrl")!.innerText = profile.images[0]?.url ?? '(no profile image)';
+}
 }
