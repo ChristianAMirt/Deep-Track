65c01173113038ef2bb38dfcc5a0a88f3af84a6f
Christian-Mirt <cmirt001@odu.edu>
2025-01-30 22:37:38 -0500

    App rename and other changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Spotify-Helper' title`, () => {
+  it(`should have the 'Deep-Track' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Spotify-Helper');
+    expect(app.title).toEqual('Deep-Track');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Spotify-Helper');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Deep-Track');
   });
 });
