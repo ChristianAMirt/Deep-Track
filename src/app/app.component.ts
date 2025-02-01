1e1d5c5a13546a1a10a1fd93cacf5b3aedd3f615
Christian-Mirt <cmirt001@odu.edu>
2025-01-31 22:19:45 -0500

    Service refactor to handle access token internally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Artist, TopArtistsResponse } from './interfaces/artist';
 import { SpotifyService } from './services/spotify/spotify.service';
 import { Profile } from './interfaces/profile';
 import { NgFor, NgIf } from '@angular/common';
-import { AuthResponse } from './interfaces/auth-response';
 import { PlayerState } from './interfaces/player-state';
 import { Subscription, switchMap, timer } from 'rxjs';
 
@@ -25,57 +24,44 @@ export class AppComponent implements OnInit, OnDestroy {
 
   /** @inheritdoc */
   ngOnInit() {
-    const params = new URLSearchParams(window.location.search);
-    const code = params.get('code');
-
-    const currentUrl = window.location.href;
-    var redirectUri = '';
-    if (currentUrl.includes('localhost')) {
-      redirectUri = 'http://localhost:4200/callback';
-    } else if (currentUrl.includes('github.io')) {
-      redirectUri = 'https://christianamirt.github.io/Deep-Track/callback';
-    } else {
-      console.log('Running on another environment');
-    }
-
-    // Check if user has already authenticated and redirect them if not
-    if (code) {
-      this.spotifyService.getAuthData(redirectUri, code).subscribe({
-        next: (authResponse: AuthResponse) => {
-          // Get user profile
-          this.spotifyService.getUserProfile(authResponse.access_token).subscribe({
-            next: (profile: Profile) => {
-              this.profile = profile;
-            },
-            error: (err) => console.error('Error fetching user profile:', err),
-          });
-          // Get user top artists
-          this.spotifyService.getTop5Artists(authResponse.access_token).subscribe({
-            next: (artistsResponse: TopArtistsResponse) => {
-              this.topArtists = artistsResponse.items;
-            },
-            error: (err) => console.error('Error fetching top artists:', err),
-          });
-          // Get player state and poll the API every 1 seconds
-          this.subscription = timer(0, 1000)
-          .pipe(switchMap(() => this.spotifyService.getPlayerState(authResponse.access_token))).subscribe({
-            next: (playerState: PlayerState) => {
-              if (this.playerState === null || playerState === null || playerState.item.id !== this.playerState.item.id) {
-                this.playerState = playerState;
-                console.log('Player State reassigned');
-              }
-            },
-            error: (err) => {
-              console.error('Error fetching top artists:', err);
-              this.playerState = null;
-            },
-          });
-        },
-        error: (err) => console.error('Error during authentication:', err),
-      });
-    } else {
-      this.spotifyService.redirectToAuthCodeFlow(redirectUri);
-    }
+    // Get user profile
+    this.spotifyService.getUserProfile().subscribe({
+      next: (profile: Profile) => {
+        this.profile = profile;
+      },
+      error: (err) => {
+        console.error('Error fetching user profile:', err);
+        this.spotifyService.redirectToAuthCodeFlow();
+      }
+    });
+    // Get user top artists
+    this.spotifyService.getTop5Artists().subscribe({
+      next: (artistsResponse: TopArtistsResponse) => {
+        this.topArtists = artistsResponse.items;
+      },
+      error: (err) => {
+        console.error('Error fetching top artists:', err);
+        this.spotifyService.redirectToAuthCodeFlow();
+      }
+    });
+    // Get player state and poll the API every 1 seconds
+    this.subscription = timer(0, 1000)
+    .pipe(switchMap(() => this.spotifyService.getPlayerState())).subscribe({
+      next: (playerState: PlayerState) => {
+        if (playerState) {
+          if (this.playerState === null || playerState.item.id !== this.playerState.item.id) {
+            this.playerState = playerState;
+          }
+        } else {
+          this.playerState = null;
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching player state', err);
+        this.playerState = null;
+        this.spotifyService.redirectToAuthCodeFlow();
+      },
+    });
   }
 
   /** @inheritdoc */
--- src/app/services/spotify/spotify.service.ts
+++ src/app/services/spotify/spotify.service.ts
@@ -1,11 +1,9 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/internal/Observable';
-import { from } from 'rxjs/internal/observable/from';
-import { map } from 'rxjs/internal/operators/map';
+import { BehaviorSubject, from, map, Observable, of, switchMap } from 'rxjs';
 import { AuthResponse } from '../../interfaces/auth-response';
 import { Profile } from '../../interfaces/profile';
-import { Artist, TopArtistsResponse } from '../../interfaces/artist';
+import { TopArtistsResponse } from '../../interfaces/artist';
 import { PlayerState } from '../../interfaces/player-state';
 
 @Injectable({
@@ -13,8 +11,7 @@ import { PlayerState } from '../../interfaces/player-state';
 })
 export class SpotifyService {
   clientId: string = '8c075a7f139146519b4e9fac7ce3439d'; // Add your client ID
-  profile: Profile | null = null;
-  authResponse: AuthResponse | null = null;
+  private authResponse$: BehaviorSubject<AuthResponse | null> = new BehaviorSubject<AuthResponse | null>(null);
 
   constructor(private http: HttpClient) {}
 
@@ -23,7 +20,13 @@ export class SpotifyService {
    * Creates encrption key pair and redirects user to Spotify authetication.
    * @param clientId The ID of the users Spotify developer account.
    */
-  redirectToAuthCodeFlow(redirectUri: string): void {
+  redirectToAuthCodeFlow(): void {
+    // Detect environment and set redirect URI
+    const redirectUri = this.getRedirectUri();
+    if (!redirectUri) {
+      throw new Error('Environment not recognized.');
+    }
+    
     const verifier = this.generateCodeVerifier(128);
 
     // Create a key/value for the code verifier and store in browser storage
@@ -72,7 +75,6 @@ export class SpotifyService {
     .post<AuthResponse>('https://accounts.spotify.com/api/token', body, { headers })
     .pipe(
       map((authResponse) => {
-        this.authResponse = authResponse; // Cache the authResponse
         return authResponse;
       })
     );
@@ -119,18 +121,112 @@ export class SpotifyService {
       })
     );
   }
+
+  /**
+   * Determines the appropriate redirect URI based on the environment.
+   * @returns The correct URI for the environment or null.
+   */
+  private getRedirectUri(): string | null {
+    const currentUrl = window.location.href;
+    if (currentUrl.includes('localhost')) {
+      return 'http://localhost:4200/callback';
+    } else if (currentUrl.includes('github.io')) {
+      return 'https://christianamirt.github.io/Deep-Track/callback';
+    }
+    return null; // Unknown environment
+  }
+
+  /**
+   * Get access token from cache or fetch a new one if needed.
+   * @returns An observable of the access token.
+   */
+  private getAccessToken(): Observable<string> {
+    // First check service property for value
+    const cachedAuth = this.authResponse$.getValue();
+
+    if (cachedAuth && cachedAuth.access_token) {
+      return of(cachedAuth.access_token);
+    }
+
+    // If no cached token, attempt to retrieve from localStorage
+    const storedAuth = localStorage.getItem('authResponse');
+    if (storedAuth) {
+      const parsedAuth = JSON.parse(storedAuth) as AuthResponse;
+      this.authResponse$.next(parsedAuth);
+      return of(parsedAuth.access_token);
+    }
+
+    // If token not stored, check URL for code, and try to make a request for access token (case where user was redirected)
+    const params = new URLSearchParams(window.location.search);
+    const code = params.get('code');
+    if (code) {
+      // Detect environment and set redirect URI
+      const redirectUri = this.getRedirectUri();
+      if (!redirectUri) {
+        return of('').pipe(
+          switchMap(() => {
+            throw new Error('Environment not recognized.');
+          })
+        );
+      }
+      return this.getAuthData(redirectUri, code).pipe(
+        map(authResponse => {
+          // Cache in service and local storage
+          this.authResponse$.next(authResponse);
+          localStorage.setItem('authResponse', JSON.stringify(authResponse));
+          return authResponse.access_token;
+        })
+      );
+    }
+
+    // If no stored token, return an error
+    return of('').pipe(
+      switchMap(() => {
+        throw new Error('No access token available. User may need to log in.');
+      })
+    );
+  }
   // #endregion
 
   /**
+   * Template function to make API calls.
+   * @param url The URL to fetch data from.
+   * @returns The requested record.
+   */
+  get<T>(url: string): Observable<T> {
+    return this.getAccessToken().pipe(
+      switchMap(token => {
+        if (!token) {
+          throw new Error('No access token available.');
+        }
+
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        });
+
+        return this.http.get<T>(url, { headers });
+      })
+    );
+  }
+
+  /**
    * Retrives a users profile from Spotify.
-   * @param accessToken The token require for API call.
    * @returns An observer of the Profile.
    */
-  getUserProfile(accessToken: string): Observable<Profile> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${accessToken}`
-    });
-    return this.http.get<Profile>('https://api.spotify.com/v1/me', { headers });
+  getUserProfile(): Observable<Profile> {
+    return this.getAccessToken().pipe(
+      switchMap(token => {
+        if (!token) {
+          throw new Error('No access token available.');
+        }
+
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        });
+
+        return this.http.get<Profile>('https://api.spotify.com/v1/me', { headers });
+      })
+    );
   }
 
   /**
@@ -138,17 +234,35 @@ export class SpotifyService {
    * @param accessToken The token require for API call.
    * @returns An observer of a list of artists.
    */
-  getTop5Artists(accessToken: string): Observable<TopArtistsResponse> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${accessToken}`
-    });
-    return this.http.get<TopArtistsResponse>('https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5&offset=0', { headers });
+  getTop5Artists(): Observable<TopArtistsResponse> {
+    return this.getAccessToken().pipe(
+      switchMap(token => {
+        if (!token) {
+          throw new Error('No access token available.');
+        }
+
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        });
+
+        return this.http.get<TopArtistsResponse>('https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5&offset=0', { headers });
+      })
+    );
   }
 
-  getPlayerState(accessToken: string): Observable<PlayerState> {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${accessToken}`
-    });
-    return this.http.get<PlayerState>('https://api.spotify.com/v1/me/player', { headers });
+  getPlayerState(): Observable<PlayerState> {
+    return this.getAccessToken().pipe(
+      switchMap(token => {
+        if (!token) {
+          throw new Error('No access token available.');
+        }
+
+        const headers = new HttpHeaders({
+          Authorization: `Bearer ${token}`
+        });
+
+        return this.http.get<PlayerState>('https://api.spotify.com/v1/me/player', { headers });
+      })
+    );
   }
 }
